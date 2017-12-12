a1f5053cb3b3461164000ab5fc8b8532cdbbcae8
Korx7 <korney.xp@mail.ru>
2017-12-12 10:36:18 +0300

    Initial commit

---
+++ keanu/app/form.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Korney on 04.04.2017.
+ */
+function openForm() {
+    document.getElementById("formFan").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("formFan").style.display = "none";
+}
